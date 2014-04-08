6cc68c0f6fa3872998f5a3241a63b4ba6d95d138
stomicic <stomicic@gmail.com>
2014-04-08 14:34:28 +0200

    v1

---
+++ server.js
@@ -0,0 +1,26 @@
+/// <reference path="../DefinitelyTyped/node/node.d.ts" />
+/// <reference path="../DefinitelyTyped/express/express.d.ts" />
+/// <reference path="./customer.ts" />
+var express = require("express");
+var cust = require("./customer");
+
+var app = express();
+
+app.get("/customer/:id", function (req, resp) {
+    var customerId = req.params.id;
+    var c = new cust.customer.Customer({ firstName: "Win #" + customerId.toString(), lastName: "Days" });
+    console.log(c.fullName());
+    resp.send(JSON.stringify(c));
+});
+
+app.get("/customer", function (req, resp) {
+    var customers;
+    customers = new Array();
+    for (var i = 0; i < 10; i++) {
+        customers.push(new cust.customer.Customer({ firstName: "Win #" + i.toString(), lastName: "Days" }));
+    }
+    resp.send(JSON.stringify(customers));
+});
+
+var port = process.env.PORT || 1337;
+app.listen(port);
